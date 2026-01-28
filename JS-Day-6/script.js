59220a7c0f2746f17a065463f88b24d5765bb457
Savita Sharma <savitasharma@example.com>
2026-01-28 15:47:42 +0530

    JS-Day-6 Done

---
+++ JS-Day-6/script.js
@@ -0,0 +1,18 @@
+// CRUD - create-read-update-delete
+// const headingevent = document.getElementById('myHeading');
+// console.log(headingevent);
+
+// headingevent . textContent="New Heading";
+// headingevent . style.color = "Blue";
+// headingevent.style.backgroundColor = "Yellow";
+
+
+const paragraph = document.querySelector("#content p");
+
+// Change the text content of the selected paragraph..
+
+paragraph.textContent = " This paragraph has been updated using query selector.";
+
+//Change the the style of the selected  paragraph.
+
+paragraph.style.color = "blue"
