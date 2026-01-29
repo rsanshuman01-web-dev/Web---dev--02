bd616772bac7ddc92231511b6dbe4ef19aa8f78c
Savita Sharma <savitasharma@example.com>
2026-01-29 11:55:06 +0530

    Add task-1 files

---
+++ Task-1/script.js
@@ -0,0 +1,18 @@
+// const profile = document.getElementById("profile");
+// profile.style.backgroundColor = " lightblue";
+// profile.style.padding = "2px";
+// profile.style.border = "5px , dashed , red";
+// profile.style.textAlign = "center";
+
+
+const imp = document.getElementsByClassName("important");
+console.log(imp.length)
+
+for(let i=0 ; i<imp.length ; i++){
+    imp[i].style.color = "red";
+    imp[i].style.fontSize = "60px";
+    
+}
+
+
+
